e53a117a321b403dac032693a37d5f7eb4773dc3
alex-prog <coag@kea.dk>
2018-09-28 10:17:06 +0200

    TCP solution for Server-Client communication

--- src/week39/tcpex/TCPServer.java
+++ src/week39/tcpex/TCPServer.java
@@ -1,6 +1,8 @@
 package week39.tcpex;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,17 +11,37 @@ import java.net.Socket;
  */
 public class TCPServer {
     public static void main(String[] args) {
+        System.out.println("=============SERVER==============");
 
-        final int PORT_IN=5656;
-        final int PORT_OUT=5657;
+        final int PORT_LISTEN = 5656;
 
 
         try {
-            ServerSocket server = new ServerSocket(PORT_IN);
+            ServerSocket server = new ServerSocket(PORT_LISTEN);
+
+            System.out.println("Server starting...\n");
+
 
             Socket socket = server.accept();
+            System.out.println("Client connected");
+            String clientIp = socket.getInetAddress().getHostAddress();
+            System.out.println("IP: " + clientIp);
+            System.out.println("PORT: " + socket.getPort());
+
+            InputStream input = socket.getInputStream();
+            OutputStream output = socket.getOutputStream();
+
+            byte[] dataIn = new byte[1024];
+            input.read(dataIn);
+            String msgIn = new String(dataIn);
+            msgIn = msgIn.trim();
 
 
+            System.out.println("IN -->" + msgIn + "<--");
+
+            String msgToSend = "SERVER: [sender:" + clientIp + " ]: " + msgIn;
+            byte[] dataToSend = msgToSend.getBytes();
+            output.write(dataToSend);
 
         } catch (IOException e) {
             e.printStackTrace();
